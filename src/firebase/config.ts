68ab83eb1845fb871428cfed7e0fec56ec2fcb30
AI마스터 <aimaster@kshcm.net>
2025-12-14 04:30:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8176556433-7698a",
+  "appId": "1:89517826209:web:1657d2595be5622d1850e0",
+  "apiKey": "AIzaSyD98EXwu0qawhpLkL8fMe1erS5aBpXzv8w",
+  "authDomain": "studio-8176556433-7698a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "89517826209"
+};
